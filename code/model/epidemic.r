574573cc671c832d2d082ad8b8394d1e8500f4bf
jessexknight <jesse.x.knight@gmail.com>
2022-08-03 14:31:10 -0700

    code: fix bug

--- code/model/epidemic.r
+++ code/model/epidemic.r
@@ -28,7 +28,7 @@ epi.init.state = function(P){ # "X"
 
 epi.array.init = function(P,t){
   # large, complete representation: t (rows) x individuals (cols)
-  A = dn.array(list('t'=t,'i'=seqn(P$N),character())
+  A = dn.array(list('t'=t,'i'=seqn(P$N)),character())
 }
 
 epi.array.update = function(A,X,tj){
