3ac558af9081e495eab3917445fa25497057feb8
jessexknight <jesse.x.knight@gmail.com>
2022-08-25 08:34:20 -0400

    code: small clean-up vt0 for scinet

--- code/model/do/vax-t0/main.r
+++ code/model/do/vax-t0/main.r
@@ -7,13 +7,13 @@ source('model/plot.r')
 
 # config - TODO: surface stuff here too?
 .debug = FALSE
-def = list()
-def$tf = 360
-def$N.s = 100
-def$N = 1000
-def$N.v = c(1000,3000,10000,30000)
-def$vax.cov.v = c(.2,.4,.6,.8)
-def$vax.eff.v = c(.2,.4,.6,.8)
+vt0 = list()
+vt0$tf = 360
+vt0$N.s = 100
+vt0$N = 1000
+vt0$N.v = c(1000,3000,10000,30000)
+vt0$vax.cov.v = c(.2,.4,.6,.8)
+vt0$vax.eff.v = c(.2,.4,.6,.8)
 
 .clean.out.long = function(out.long,N.v,vax.cov.v,vax.eff.v){
   out.long$N       = factor(out.long$N,N.v,paste0(N.v))
@@ -24,7 +24,7 @@ def$vax.eff.v = c(.2,.4,.6,.8)
 
 vt0.run = function(seeds,N,vax.cov,vax.eff){
   print(sprintf('N = %d, cov = %.2f, eff = %.2f',N,vax.cov,vax.eff))
-  t.vec = epi.t(tf=def$tf)
+  t.vec = epi.t(tf=vt0$tf)
   P.s = def.params.s(seeds,N=N,N.V0=c(round(N*vax.cov),0),vax.eff.dose=c(vax.eff,0))
   E.s = epi.run.s(P.s,t.vec)
   out.long.s = cbind(rbind(
@@ -72,38 +72,36 @@ vt0.plot.tex = function(out.long){
   # stupid hack as ecdf does not support NA
   noise = function(x){ x + runif(len(x),-1e-9,+1e-9) } # avoid unique
   cdf.adj = function(y,...){ unlist(lapply(split(y,list(...)),function(yi){
-    yi * (len(yi)-2) / def$N.s # only non-NA in yi, so we use len(yi) vs N.s to adjust; -2 from pad
+    yi * (len(yi)-2) / vt0$N.s # only non-NA in yi, so we use len(yi) vs N.s to adjust; -2 from pad
   })) }
   g = ggplot(row.select(out.long,var='tex'),aes(x=noise(value),color=vax.eff,fill=vax.eff)) +
     stat_ecdf(aes(y=after_stat(cdf.adj(y,group,PANEL)))) +
     facet_grid('vax.cov ~ N') +
     theme_light() +
-    lims(x=c(0,def$tf)) +
+    lims(x=c(0,vt0$tf)) +
     scale_color_viridis(discrete=TRUE) + scale_fill_viridis(discrete=TRUE) +
     labs(color='',fill='',y='Cumulative Probability of Extinction',x='Time to Extinction (days)')
-  fig.save('vt0-plot-tex',w=8,h=6)
+  fig.save('vt0-plot-tex',w=10,h=8)
 }
 
 vt0.surface.cia = function(N.s=10,N.grid=7){
   # run the grid
   vax.cov.v = seq(0,1,l=N.grid)
   vax.eff.v = seq(0,1,l=N.grid)
-  out.long = vt0.run.grid(N.s,def$N,vax.cov.v,vax.eff.v)
-  out.long = row.select(out.long,var='cia',t=def$tf-1)
+  out.long = vt0.run.grid(N.s,vt0$N,vax.cov.v,vax.eff.v)
+  out.long = row.select(out.long,var='cia',t=vt0$tf-1)
   out.long.agg = aggregate(formula('value ~ vax.cov + vax.eff'),out.long,median)
   # plot
   g = ggplot(out.long.agg,aes_string(x='100*vax.cov',y='100*vax.eff',z='value')) +
     geom_contour_filled(alpha=.8) +
     theme_light() +
     labs(x='Coverage (%)',y='Effect. (%)',fill='CIA (%)')
-  fig.save('vt0-surface-cia',w=6,h=5)
+  fig.save('vt0-surface-cia',w=10,h=8)
 }
-
-out.long = do.call(vt0.run.grid,def[c('N.s','N.v','vax.cov.v','vax.eff.v')])
-out.long = .clean.out.long(out.long,def$N.v,def$vax.cov.v,def$vax.eff.v)
-vt0.plot.cia(out.long)
-vt0.plot.tex(out.long)
-vt0.surface.cia()
-
-
-
+if (sys.nframe() == 0){
+  out.long = do.call(vt0.run.grid,vt0[c('N.s','N.v','vax.cov.v','vax.eff.v')])
+  out.long = .clean.out.long(out.long,vt0$N.v,vt0$vax.cov.v,vt0$vax.eff.v)
+  vt0.plot.cia(out.long)
+  vt0.plot.tex(out.long)
+  vt0.surface.cia()
+}
