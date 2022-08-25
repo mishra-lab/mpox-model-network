6719d9a8f09c03b8129b57b0d2e43d1e1e2e2036
jessexknight <jesse.x.knight@gmail.com>
2022-08-25 14:18:59 -0400

    code: scinet fix (maybe)

--- code/scinet/main.r
+++ code/scinet/main.r
@@ -5,6 +5,7 @@ source('model/do/vax-t0/main.r')
 vt0$N.s = 1000
 # run + plot
 out.long = do.call(vt0.run.grid,vt0[c('N.s','N.v','vax.cov.v','vax.eff.v')])
+save(file=vt0.fname.rdata('out-long'),out.long)
 out.long = .clean.out.long(out.long,vt0$N.v,vt0$vax.cov.v,vt0$vax.eff.v)
 vt0.plot.cia(out.long)
 vt0.plot.tex(out.long)
