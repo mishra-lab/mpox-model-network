b2d1e83368cdc34f640437f6ab601997270da196
jessexknight <jesse.x.knight@gmail.com>
2022-09-02 11:39:19 -0400

    code: update scinet

--- code/scinet/main.r
+++ code/scinet/main.r
@@ -1,11 +1,17 @@
 # load
-source('model/do/vax-t0/main.r')
+source('model/do/vt0/main.r')
 # config
 .n.cores = 80
-vt0$N.s = 1000
+vt0$N.s = 100
 # run + plot
 out.long = do.call(vt0.run.grid,vt0[c('N.s','N.v','vax.cov.v','vax.eff.v')])
 save(file=vt0.fname.rdata('out-long'),out.long)
+load(file=vt0.fname.rdata('out-long'))
 out.long = .clean.out.long(out.long,vt0$N.v,vt0$vax.cov.v,vt0$vax.eff.v)
-vt0.plot.cia(out.long)
-vt0.plot.tex(out.long)
+par.funs(out.long=out.long,list( # generate plots in parallel
+  list(vt0.plot.var,var='cia'),
+  list(vt0.plot.var,var='inc',health='all'),
+  list(vt0.plot.var,var='prev',health='I'),
+  list(vt0.plot.tex)
+))
+
